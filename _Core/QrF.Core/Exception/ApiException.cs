15e74f3c04f43bcf260383f02fb7a9b84aced732
ren8179 <ren8179@126.com>
2016-05-03 14:24:58 +0800

    添加一些基础工具类

---
+++ _Core/QrF.Core/Exception/ApiException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QrF.Core
+{
+    public class ApiException : Exception
+    {
+        public string ErrorCode { get; set; }
+        public ApiException(string message, string errorCode)
+            : base(message)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
